ae00e0cc61f89e7d08c8e11d31cff502519b916b
JungInBaek <bji1527@naver.com>
2021-10-31 21:31:21 +0900

    애플리케이션 배너 변경, 컴포넌트 스캔 설정, 롬복 설정

--- src/main/java/com/rubypaper/Chapter01Application.java
+++ src/main/java/com/rubypaper/Chapter01Application.java
@@ -1,10 +1,13 @@
 package com.rubypaper;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rubypaper", "com.ruby"})
 public class Chapter01Application {
 
 	public static void main(String[] args) {
@@ -12,6 +15,10 @@ public class Chapter01Application {
 		SpringApplication application = new SpringApplication(Chapter01Application.class);
 
 		application.setWebApplicationType(WebApplicationType.SERVLET);
+
+		//	배너 OFF
+		application.setBannerMode(Banner.Mode.OFF);
+
 		application.run(args);
 	}
 
